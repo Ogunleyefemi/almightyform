1eba8546c6f2a3dc8bb06ef5734c708c67514c66
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-05-21 16:43:50 +0100

    add equation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let a = parseInt(prompt( "put value for a"));
+let b = parseInt(prompt( "put value for b"));
+let c = parseInt(prompt( "put value for c"));
+function almightyform(){
+    return ((- b )+ Math.sqrt(b**2 - 4 * a * c)) / 2 * a
+}
+document.write(almightyform());
