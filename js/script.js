2d3cf0c4864beeb9df38efc7360095490174b3cb
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-05-22 13:03:30 +0100

    add question2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function almightyform(){
     return ((- b )+ Math.sqrt(b**2 - 4 * a * c)) / 2 * a
 }
 document.write(almightyform());
+
+//question2
+let y = parseInt(prompt( "input value for y"));
+function solveM(){
+    return (y ** 2 - 32)** 2 / Math.sqrt(4 / y);
+}
+document.write(solveM());
+
+//question3
+let a = parseInt(prompt( "input value for a"));
+let b = parseInt(prompt( "input value for b"));
+let c = parseInt(prompt( "input value for c"));
+let d = parseInt(prompt( "input value for d"));
+function solveEquation(){
+    return (a * c + b * d) / (c ** 2 + d ** 2) + (b * c - a * d) / (c ** 2 + d ** 2);
+}
+document.write(solveEquation());
