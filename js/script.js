5fae946af053a5d4d619ec55a98f27b63ec605f6
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-05-22 14:53:54 +0100

    add math.round

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ let c = parseInt(prompt( "put value for c"));
 function almightyform(){
     return ((- b )+ Math.sqrt(b**2 - 4 * a * c)) / 2 * a
 }
-document.write(almightyform());
+document.write(Math.round(almightyform()));
 
 //question2
-let y = parseInt(prompt( "input value for y"));
-function solveM(){
-    return (y ** 2 - 32)** 2 / Math.sqrt(4 / y);
-}
-document.write(solveM());
+// let y = parseInt(prompt( "input value for y"));
+// function solveM(){
+//     return (y ** 2 - 32)** 2 / Math.sqrt(4 / y);
+// }
+// document.write(Math.round(solveM()));
 
 //question3
-let a = parseInt(prompt( "input value for a"));
-let b = parseInt(prompt( "input value for b"));
-let c = parseInt(prompt( "input value for c"));
-let d = parseInt(prompt( "input value for d"));
-function solveEquation(){
-    return (a * c + b * d) / (c ** 2 + d ** 2) + (b * c - a * d) / (c ** 2 + d ** 2);
-}
-document.write(solveEquation());
+// let a = parseInt(prompt( "input value for a"));
+// let b = parseInt(prompt( "input value for b"));
+// let c = parseInt(prompt( "input value for c"));
+// let d = parseInt(prompt( "input value for d"));
+// function solveEquation(){
+//     return (a * c + b * d) / (c ** 2 + d ** 2) + (b * c - a * d) / (c ** 2 + d ** 2);
+// }
+// document.write(Math.round(solveEquation()));
